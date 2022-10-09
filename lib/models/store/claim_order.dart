80d434860e47310cad14519c6ca9af32293a7e54
keval-makadiya <keval4105@gmail.com>
2022-10-09 22:31:09 +0530

    added: claim_item model

--- lib/models/store/claim_order.dart
+++ lib/models/store/claim_order.dart
@@ -1,3 +1,14 @@
 class ClaimOrder {
   //TODO: ClaimOrder add fields, fromJson, toJson
+
+  ClaimOrder();
+
+  ClaimOrder.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
--- lib/models/store/product_variant.dart
+++ lib/models/store/product_variant.dart
@@ -1,3 +1,14 @@
+import 'package:medusa_flutter/models/store/product.dart';
+
 class ProductVariant {
   //TODO: ProductVariant: add fields, fromJson, toJson
+
+  ProductVariant.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
