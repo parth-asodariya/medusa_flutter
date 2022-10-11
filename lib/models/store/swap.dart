65439f18c8e1a035afb18b29b2b3ba625ad75382
keval-makadiya <keval4105@gmail.com>
2022-10-11 22:28:26 +0530

    added: multiple response models

--- lib/models/store/swap.dart
+++ lib/models/store/swap.dart
@@ -1,3 +1,12 @@
 class Swap {
   //TODO: Swap: add fields, fromJson, toJson
+
+  Swap.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
