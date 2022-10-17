cc58efd19c56e755cdec7777dce71143f2ca5d34
keval-makadiya <keval4105@gmail.com>
2022-10-17 22:37:19 +0530

    added: new models for response parsing

--- lib/models/store/order_edit.dart
+++ lib/models/store/order_edit.dart
@@ -1,3 +1,15 @@
 class OrderEdit {
   //TODO: OrderEdit: add fields, fromJson, toJson
+
+  OrderEdit();
+
+  OrderEdit.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+
+    return json;
+  }
 }
