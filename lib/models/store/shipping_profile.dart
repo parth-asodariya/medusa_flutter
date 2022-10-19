9b0778611e5c83ef393aed5ce62163dc996f8463
keval-makadiya <keval4105@gmail.com>
2022-10-19 23:18:28 +0530

    added: new models for response parsing

--- lib/models/store/shipping_profile.dart
+++ lib/models/store/shipping_profile.dart
@@ -1,3 +1,14 @@
 class ShippingProfile {
   //TODO: ShippingProfile: add fields, fromJson, toJson
+
+  ShippingProfile();
+
+  ShippingProfile.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
--- lib/models/store/tax_rate.dart
+++ lib/models/store/tax_rate.dart
@@ -1,3 +1,14 @@
 class TaxRate {
   //TODO: TaxRate: add fields, fromJson, toJson
+
+  TaxRate();
+
+  TaxRate.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
