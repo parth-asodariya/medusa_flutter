47d2ebdbbe262c22d11b4688f3fa41d1d2c7b612
keval-makadiya <keval4105@gmail.com>
2022-10-20 20:53:34 +0530

    added: new models for response parsing

--- lib/models/store/tax_provider.dart
+++ lib/models/store/tax_provider.dart
@@ -1,3 +1,14 @@
 class TaxProvider {
   //TODO: TaxProvider: add fields, fromJson, toJson
+
+  TaxProvider();
+
+  TaxProvider.fromJson(Map<String, dynamic> json) {
+    //
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = <String, dynamic>{};
+    return json;
+  }
 }
